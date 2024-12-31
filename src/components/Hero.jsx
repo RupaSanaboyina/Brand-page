289cf7c61e6d83a60db3a328c5376cf8f9068e0f
Rupa Lakshmi Prasanna <rupa@Rupa.local>
2024-12-31 12:03:30 +0530

    First Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+const Herosection=()=>{
+    return(
+        <main className="hero">
+            <div className="hero-content"> 
+                <h1 className="heading">YOUR FEET 
+                DESERVE THE BEST</h1>
+                <p className=""> YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES </p>
+           <div className="hero-buttons">
+            <button className="shopNow">Shop Now</button>
+            <button className="Category">Category</button>
+            </div>
+                <div className="shopping">
+                <p className="availble">Avaible on</p>
+                <div className="brand-icons">
+                <img src="images/amazon.png" alt="amazon"/>
+                <img src="images/flipkart.png" alt="flipkart"/>
+                </div>
+                </div>
+                </div>
+                            
+                <div className="hero-image"> 
+                <img src="images/shoe_image.png" alt="shoe"/>
+
+            </div>
+        </main>
+    )
+}
+export default Herosection;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation=()=>{
+    return(
+<nav>
+                <div className="">
+                    <img src="images/brand_logo.png" alt="Nike"/>
+
+                </div>
+                <ul>
+                    <li href="#">Menu</li>
+                    <li href="#">Location</li>
+                    <li href="#">About</li>
+                    <li href="#">Contact</li>
+
+                </ul>
+                <button>Login</button>
+            </nav>
+    )
+}
+export default Navigation;
